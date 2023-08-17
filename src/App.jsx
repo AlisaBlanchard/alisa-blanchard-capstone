8f713fa6d5797caa4ceee979e1127bc9503c45a8
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-17 14:54:23 -0400

    - [x]  Make new branch for TAS 23 - Create User Dashboard Page Component - [x]  Create user dashboard component js & scss - [x]  Import new component to app.js so you can visualize the changes you make - [x]  Sketch out basic component structure - [x]  sketch out scss structure - [x]  give elements colour to differentiate styling - [x]  Style for mobile view - [x]  update variable colours - [x]  Add dynamic variables to component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home/home';
+// import Home from './pages/home/home';
+import UserDashboard from './components/userDashboard/userDashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Home />} />
+          {/* <Route path='/' element={<Home />} /> */}
+          <Route path='/' element={<UserDashboard />} />
           <Route path='' element='' />
           <Route path='' element='' />
           <Route path='' element='' />
