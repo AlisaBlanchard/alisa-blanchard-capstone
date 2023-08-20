48eefeb7685f473d5c3e32915504abdebd5ca2ae
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-19 21:39:17 -0400

    - [x]  Make GET request to get users array - [x]  use dynamic variables to fill the form

--- src/pages/userProfile/userProfile.jsx
+++ src/pages/userProfile/userProfile.jsx
@@ -1,14 +1,46 @@
+import axios from 'axios';
 import '../userProfile/userProfile.scss';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 //UserProfile Page
 
 function UserProfile() {
+    //State
+    const [details, setDetails] = useState({});
+    console.log(details);
+
+    const { userId } = useParams();
+
+    //GET to retrieve Users array
+    useEffect(() => {
+        const URL = "https://localhost:5050";
+
+        axios
+        .get(`${URL}/users/`)
+
+        .then((res) => {
+            const users = res.data;
+            console.log(res.data);
+
+            const foundUser = users.find((user) => user.userId == userId);
+            
+            setDetails(foundUser);
+        });
+    }, []);
+
+    console.log(details);
+
+
     return (
         <section className="userProfile">
             <div className="user__wrap">
                 <img src="#" alt="Profile Picture" />
                 <div className="user__info">
-                    <p>User Info</p>
+                    {/* <p>Username: {details.username}</p>
+                    <p>Name: {details.name}</p>
+                    <p>Birthday: {details.birthdate}</p>
+                    <p>Sessions: {details.tracking_sessions}</p> */}
                 </div>
             </div>
             <div className="data__wrap">
