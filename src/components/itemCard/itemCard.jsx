f2e14ce1e06ea7e41e9dc8fa78f34aaf1fb28a29
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-22 18:05:36 -0400

    commit

--- src/components/itemCard/itemCard.jsx
+++ src/components/itemCard/itemCard.jsx
@@ -3,6 +3,11 @@ import '../itemCard/itemCard.scss';
 // Item Card Component
 
 function ItemCard() {
+    // State
+
+    // GET to get Merch item array
+
+
     return(
         <div className="itemCard">
             <img src="" alt="item image" />
