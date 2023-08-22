40401b9365a9c1a8218ca8fcbf1a3d43efbfa5c6
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-22 12:20:52 -0400

    - [x]  Create new branch for TAS 51 Create Shop Page - [x]  Create Shop jsx & scss - [x]  Add Shop to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import UserDashboard from './components/userDashboard/userDashboard';
 import HeaderNav from './components/headerNav/headerNav';
 import UserProfile from './pages/userProfile/userProfile';
+import Shop from './pages/shop/shop';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path='/' element={<UserDashboard />} />
           {/* User Profile */}
           <Route path='/profile/:userId' element={<UserProfile />} />
-          <Route path='' element='' />
+          {/* Shop */}
+          <Route path='/shop' element={<Shop />} />
           <Route path='' element='' />
           <Route path='' element='' />
           <Route path='' element='' />
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,13 @@
+import '../shop/shop.scss';
+
+//Shop Page 
+
+function Shop() {
+    return(
+        <section className="shop">
+
+        </section>
+    )
+}
+
+export default Shop;
