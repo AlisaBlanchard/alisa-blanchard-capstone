0366f943152370316642cc14b438f412733f1ccf
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-22 17:39:54 -0400

    - [x]  Sketch out element structure for shop page - [x]  Sketch out scss to reflect element structure - [x]  style for mobile view - [x]  add media queries - [x]  style for tablet view - [x]  style for desktop view

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -5,6 +5,15 @@ import '../shop/shop.scss';
 function Shop() {
     return(
         <section className="shop">
+            <div className="hero">
+                <h1>Call To Action</h1>
+                <p>Hero Text about quality of life x products you can buy below!</p>
+            </div>
+            <div className="merch">
+                {/* CARD COMPONENT HERE*/}
+                <div className="card">Item Card 1</div>
+                <div className="card">Item Card 2</div>
+            </div>
 
         </section>
     )
