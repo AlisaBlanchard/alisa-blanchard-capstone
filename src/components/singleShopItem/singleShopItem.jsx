ec6f3216f22715455af30274c4801e4e253faf20
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-29 16:04:38 -0400

    - [x]  Sketch out single merch item component element structure - [x]  sketch out structure in scss that reflects jsx changes - [x]  style for mobile

--- src/components/singleShopItem/singleShopItem.jsx
+++ src/components/singleShopItem/singleShopItem.jsx
@@ -4,7 +4,32 @@ import '../singleShopItem/singleShopItem.scss';
 
 function SingleShopItem() {
     return(
-        <section className="item__info">
+        <section className="single__item">
+            <div className="back__wrap">
+                <img src="" alt="back arrow" />
+                <h3>Back to Shopping</h3>
+            </div>
+            <div className="item__info--wrap">
+                <div className="image">
+                    <img src="" alt="Item Image" />
+                </div>
+
+                <div className="item__info">
+                    <h1>Item Name</h1>
+                    <h2>$19.99</h2>
+                    <p>Description of item from api lorem ipsum</p>
+                    <h3>Sizes</h3>
+                    <div className="size__wrap">
+                        <div>XS</div>
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                    </div>
+                    <button className="add__item">Add to Bag</button>
+
+                </div>
+            </div>
 
         </section>
     )
