499a3b7cffb1f4955fefc3d4d4fbf64ff8a0fe2c
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-09-14 20:15:42 -0400

    - [x]  Fix backend connections for user profile - [x]  add get to retrieve tracker information - [x]  Add tracker dropdown logic

--- src/pages/userProfile/userProfile.jsx
+++ src/pages/userProfile/userProfile.jsx
@@ -9,7 +9,11 @@ import AuthHoc from '../../components/AuthHoc/AuthHoc';
 function UserProfile() {
     //State
     const [details, setDetails] = useState({});
-    // console.log(details);
+    const [trackerDetails, setTrackerDetails] = useState([]);
+    const [tracker, setTracker] = useState({});
+
+    
+    // console.log(JSON.stringify(details.profPic));
 
     const { userId } = useParams();
 
@@ -21,7 +25,7 @@ function UserProfile() {
         .get(`${URL}/users`)
 
         .then((res) => {
-            const users = res.data;
+            const users = res.data.users;
             // console.log(res.data);
 
             const foundUser = users.find((user) => user.userId == userId);
@@ -29,6 +33,38 @@ function UserProfile() {
             setDetails(foundUser);
         });
     }, []);
+
+    //GET to retrieve all of the user's trackers by userId
+    useEffect(() => {
+        const URL = "http://localhost:5050";
+
+        axios
+        .get(`${URL}/trackers`)
+
+        .then((res) => {
+            const trackers = res.data.Trackers;
+
+            const foundTrackers = trackers.filter((user) => user.userId == userId);
+
+            const userTrackers = [];
+
+            foundTrackers.forEach((tracker) => {
+                const trackerObj = tracker;
+                if (!userTrackers.includes(trackerObj)) {
+                    userTrackers.push(trackerObj);
+                }
+            });
+
+            // console.log(userTrackers);
+
+            setTrackerDetails(userTrackers);
+        });
+    }, []);
+
+    //Changehandler for dropdown menu
+    const trackerChangeHandler = (e) => {
+        console.log(e.target);
+    }
 
     // console.log(details);
 
@@ -38,17 +74,25 @@ function UserProfile() {
             <div className="user__wrap">
             <h1>{details.username}</h1>
                 <div className="user__info--wrap">
-                    <div src="#" alt="Profile Picture" className='user__img' />
+                    <img src={details.profPic} alt="Profile Picture" className='user__img' />
                     <div className="user__info">
-                        <p>Name: {details.name}</p>
-                        <p>Birthday: {details.birthdate}</p>
-                        <p>Sessions: {details.tracking_sessions}</p>
+                        <p>Name: {details.firstName} {details.lastName}</p>
+                        {/* <p>Sessions: {details.tracking_sessions}</p> */}
                     </div>
                 </div>
             </div>
             <div className="data__wrap">
                 <div className="action__wrap">
-                    <div className="tracker__dropdown">Tracker Dropdown</div>
+                    <div className="tracker__dropdown">
+                        <select name="tracker__dropdown" id="tracker__dropdown" onChange={trackerChangeHandler}>
+                            {trackerDetails.map((tracker) => {
+                                return(
+                                    <option value={tracker.tracker_name} key={tracker.trackerId}>{tracker.tracker_name}</option>
+                                )
+                            })}
+
+                        </select>
+                    </div>
                     <div className="data__button">See My Data</div>
                 </div>
                 <div className="data__visualizer">CHARTS</div>
