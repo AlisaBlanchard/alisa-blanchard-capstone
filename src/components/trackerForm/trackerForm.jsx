e7951c060792259827549e440f0e8b3a3adc2601
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-09-16 16:03:47 -0400

    - [x]  Add changehandler with post to post new session to sessions array - [x]  add changehandler to handle changes to form inputs

--- src/components/trackerForm/trackerForm.jsx
+++ src/components/trackerForm/trackerForm.jsx
@@ -9,19 +9,28 @@ import happy from '../../assets/icons/smile.svg';
 
 //Tracker Form Component
 
-function TrackerForm() {
+function TrackerForm({userId, trackerId}) {
 
     //State
     const [tracker, setTracker] = useState({});
     const [template, setTemplate] = useState([]);
     const [input, setInput] = useState([]);
+    const [formData, setFormData] = useState({
+        trackerName: "",
+        userId: userId,
+        label1: "",
+        method1: "",
+        label2: "",
+        method2: "",
+        label3: "",
+        method3: "",
+        label4: "",
+        method4: "",
+    
+    });
+
 
     // console.log(tracker);
-
-    const userInfo = useParams();
-
-    const userId = userInfo.userId;
-    const trackerId = userInfo.trackerId;
 
     //GET to retrieve tracker info by userId & trackerId
     useEffect(() => {
@@ -41,13 +50,41 @@ function TrackerForm() {
         });
     }, []);
 
+    //Change handler to handle form submission
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        //POST to add new tracker to trackers array
+        const URL = "http://localhost:5050";
+
+        axios
+        .post(
+            `${URL}/sessions`,
+            { formData },
+            {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            }
+        )
+        .then((res) => {
+            console.log(res);
+        })
+
+        .catch((err) => {
+            console.log("Error is: " + err);
+        });
+  };
 
     //Change handler to handle form changes
+    const inputChangeHandler = (e) => {
 
-    //Change handler to submit form 
+        const { id, name, value } = e.target;
+        console.log("name:" + name);
+        console.log("value:" + value);
 
-    //POST to add new form info to specific tracker's array of data
-
+        setFormData({ ...formData, [name]: value });
+    };
 
     //Change Handler to pop up an alert to tell the user their 
         //information has been submitted and ask if they would like to 
@@ -56,7 +93,7 @@ function TrackerForm() {
 
     return (
         <section className="trackerForm">
-            <form action="submit">
+            <form action="submit" onSubmit={handleSubmitForm}>
                 <div className="form__grid--wrap">
                     <div className="grid__header">
                         <h2>{tracker.tracker_name}</h2>
@@ -69,14 +106,15 @@ function TrackerForm() {
                                 <div className="grid__label">
                                     <h3>{row.label}</h3>
                                 </div>
+                                
                                 {/* Needs to generate specific input style based on method received from template 
                                 if/else/else/else statement covering all available input options?*/}
 
                                         <div className="grid__input">
                                             <label htmlFor="yes">Yes</label>
-                                            <input type='radio' />
+                                            <input type='radio' name='Yes' value={true} onChange={inputChangeHandler}/>
                                             <label htmlFor="no">No</label>
-                                            <input type='radio' />
+                                            <input type='radio' name='No' value={false} onChange={inputChangeHandler}/>
                                         </div>
                                 {/* if (row.method == 'BooleanRadio') {
                                 }
