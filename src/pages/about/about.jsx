407bb8d6175938f20573b75ad554c02bd3ebb3e1
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-09-17 19:57:17 -0400

    Style about for all views

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -10,7 +10,6 @@ function About() {
                 <img src={aboutHero} alt="Hero Image" />
                 <div className="hero__text">
                     <h1>About Us</h1>
-                    <h2>Meaningful quote that relates to the company and their goals</h2>
                 </div>
             </div>
 
@@ -19,11 +18,7 @@ function About() {
                     <br />Our Life Tracking app empowers you with customizable tools, ensuring accurate insights for informed decisions. 
                     <br />By tailoring your tracking experience, you'll draw conclusions that drive real improvements. 
                     <br />We're your partner in this journey, helping you transform data into a better life.</p>
-                <img src="#" alt="Mission Statement Image" />
             </article>
-
-            <div className="feedback__cta"> Provide Feedback by clicking here!</div>
-
         </section>
     )
 }
